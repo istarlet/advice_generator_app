2b7745e53a88681a9b567d8f1f309ad9ab2c6aee
istarlet <istarlet@gmail.com>
2022-02-22 16:30:37 -0500

    Add updated main.js code from my codepen

--- main.js
+++ main.js
@@ -2,18 +2,13 @@ let adviceArea = document.querySelector("#adviceArea");
 let adviceNumber = document.querySelector("#adviceNumber");
 let diceBtn = document.querySelector("#diceBtn");
 
-const url = "https://api.adviceslip.com/advice";
-
-
-
-
 function adviceGenerator() {
-fetch(url)
-    .then(res => res.json())
-    .then(data => {
-        adviceNumber.textContent = `Advice #${data.slip.id}`;
-        adviceArea.textContent = `"${data.slip.advice}"`;
-    })
+  fetch("https://api.adviceslip.com/advice", { cache: "no-store" })
+    .then((response) => response.json())
+    .then((data) => {
+      adviceNumber.textContent = `Advice #${data.slip.id}`;
+      adviceArea.textContent = `${data.slip.advice}`;
+    });
 }
 
 diceBtn.addEventListener("click", adviceGenerator);
